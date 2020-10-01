fdfadb2a3c1ab1e4a697954bc12cb51743958284
techie-star <48321020+techie-star@users.noreply.github.com>
2020-10-01 19:52:03 +0530

    Update CN_CRC_Error_detection.cpp

--- CN_CRC_Error_detection.cpp
+++ CN_CRC_Error_detection.cpp
@@ -1,78 +1,102 @@
 #include<iostream>
+#include<string>
+#include<conio.h>
+#include<fstream>
+ 
 using namespace std;
-int length;
-void rev(int num[],int len)
+ 
+int main()
 {
-     int temp;
-     for(int i1=0,i2=len-1;i1<len/2;i1++,i2--)
-     {
-             temp=num[i2];
-             num[i2]=num[i1];
-             num[i1]=temp;
-     }
-     return;
-}
-void dec_to_bin(int dec,int bin_ary[])
-{
-       int tmp,i=0;
-       tmp=dec;
-       while(tmp!=0)
+    string msg;
+    int flag,i,j;
+    do
+    {
+        flag=0;
+       cout<<"\n\n Enter the message (having bits 0 or 1 only) : ";
+       cin>>msg;
+       for(i=0;i<msg.length();i++)
        {
-                    bin_ary[i]=tmp%2;
-                    tmp=tmp/2;
-                    i++;
+           if(msg[i]!='0'&&msg[i]!='1')
+            flag=1;
        }
-       rev(bin_ary,i);
-       length=i;
-       for(int a=0;a<i;a++)
-               cout<<bin_ary[a];
-       cout<<endl;
-       return;
+    }while(flag==1);
+ 
+    string divisor("1011");
+    string divisor_0("0000");
+    const int div_len=4;
+    string codeword(msg);
+    string remainder;
+    string temp_remainder;
+ 
+    for(i=0 ; i<div_len-1 ; i++)
+    {
+        codeword +='0';
+    }
+    cout<<"\n\n Codeword after appending "<<div_len-1<<" zeroes is : "<<codeword;
+ 
+    //for first division.
+    if(codeword[0]=='0')   //use divisor_0.
+    {
+        for(i=1;i<div_len;i++)
+        {
+            if(codeword[i]=='0')
+                remainder+='0';
+            else
+                remainder+='1';
+        }
+    }
+ 
+    else
+    {
+        for(i=1;i<div_len;i++)
+        {
+            if(codeword[i]==divisor[i])
+                remainder+='0';
+            else
+                remainder+='1';
+        }
+    }
+ 
+    //for further divisions.
+    for(i ; i<codeword.length() ; i++)
+    {
+        remainder+=codeword[i];
+        if(remainder[0]=='0')  //use divisor_0.
+        {
+            for(j=1;j<div_len;j++)
+            {
+                if(remainder[j]=='0')
+                    temp_remainder +='0';
+                else
+                    temp_remainder += '1';
+            }
+        }
+ 
+        else     //use divisor.
+        {
+            for(j=1;j<div_len;j++)
+            {
+                if(remainder[j]==divisor[j])
+                    temp_remainder +='0';
+                else
+                    temp_remainder += '1';
+            }
+        }
+ 
+        remainder=temp_remainder;
+        temp_remainder.clear();
+    }
+    cout<<"\n\n Remainder of the division performed is : "<<remainder;
+    codeword.replace(msg.length() , div_len-1 , remainder);
+    cout<<"\n\n Codeword is : "<<codeword;
+    cout<<"\n\n Dataword is : "<<msg;
+    cout<<"\n\n Remainder is : "<<remainder<<"\n\n";
+ 
+    ofstream fout;
+    fout.open("crc.txt",ios::out);
+    fout<<codeword;
+    fout.close();
+ 
+    getch();
+    return 0;
 }
- int sub_xor_bin(int divt[],int divr[],int len_div,int len)
- {
-      int j=0,k;
-
-      while(divt[j]!=1)
-              j++;
-      if((len-j)<len_div)
-                        return 0;
-      for(int i=j,k=0;i<(len_div+j);i++,k++)
-      {
-              if(divt[i]==divr[k])
-                                  divt[i]=0;
-              else
-                  divt[i]=1;
-      }
-      return 1;
- }
-
- void app_end(int num[],int app,int &norm)
- {
-      length=(norm+app)-1;
-      for(int i=norm-1;i<length;i++)
-              num[i]=0;
-      cout<<"The number after appending is:\n";
-      for(int i=0;i<length;i++)
-              cout<<num[i]<<" ";
-      cout<<endl;
-
-      norm=length;
- }
-
-
- void division(int msg[],int &len_msg,int gn[],int len_gn)
- {
-      int flag;
-      app_end(msg,len_gn,len_msg);
-      for(int i=0;i<len_msg-2;i++)
-      {
-              flag=sub_xor_bin(msg,gn,len_gn,len_msg);
-              if(flag==0)
-              break;
-      }
-      cout<<"The msg now after dividing:\n";
-      for(int i=0;i<len_msg;i++)
-              cout<<msg[i]<<" ";
-      cout<<endl;
- }
